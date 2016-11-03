e3109116e3681ed26ed88d7fe6d5c982b0e35f6d
sinkuu <sinkuupump@gmail.com>
2016-11-03 13:59:19 +0900

    Fix link

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! `rowcol` crate provides fixed-size [`Vector`] and [`Matrix`].
-//! Their length or number of rows/columns are provided as type parameter
+//! Their length or number of rows/columns are specified as type parameter
 //! using type-level numerics provided by `typenum` crate.
 //! This let vectors and matrices be internally represented as fixed-length arrays,
 //! therefore they are allocated on the stack, and becomes `Copy` if their content
@@ -7,7 +7,7 @@
 //! can be detected at compile-time, instead of runtime panic.
 //!
 //! [`Vector`]: ./vector/struct.Vector.html
-//! [`Matrix`]: ./vector/struct.Matrix.html
+//! [`Matrix`]: ./matrix/struct.Matrix.html
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #[cfg(not(feature = "std"))]
